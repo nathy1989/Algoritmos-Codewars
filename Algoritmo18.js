8d499efe2875f82056f750a581e23425a61765d9
nathy <nathy@spartandevs.com>
2023-07-26 20:34:12 -0400

    Se actualizan algoritmos

---
+++ Algoritmo18.js
@@ -0,0 +1,18 @@
+/*Ejercicio 18:¿Par o impar?
+
+Dada una lista de enteros, determina si la suma de sus elementos es par o impar.
+
+Da tu respuesta como una coincidencia de cadenas "odd"o "even".
+
+Si la matriz de entrada está vacía, considérela como: [0](matriz con cero).
+*/
+
+const oddOrEven = (array) => {
+    let suma = array.reduce((acc, item) => acc + item, 0)
+    return suma % 2 === 0 ? "even" : "odd"
+}
+
+console.log(oddOrEven([0]))
+console.log(oddOrEven([1]))
+console.log(oddOrEven([]))
+console.log(oddOrEven([0, 1, 2]))
